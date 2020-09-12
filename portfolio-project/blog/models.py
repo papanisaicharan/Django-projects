b5c277dcdeba906d05b11a883f55b778c311dd65
extern-papani-saicharan <extern.Papani_Saicharan@allianz.com>
2020-09-12 21:40:31 +0530

    Added bootstrap and job are done

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create A blog model here.
+class Blog(models.Model):
+    # title, pub_date, body, image
+    title = models.CharField(max_length=25)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=300)
+    image = models.ImageField(upload_to="images/")
+
+
+
+# Add the blog app to the settings
+# create a migration
+# migrate
+
+# Add to the admin
